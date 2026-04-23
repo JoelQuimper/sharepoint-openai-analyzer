04cf95d6b3dc09a02eb1bbf8c6cf10132104f6d8
Joel Quimper <joel_quimper@hotmail.com>
2026-04-23 15:08:44 -0400

    Refactor TestAgentNew and Program.cs for improved test execution and logging; update package references in TestConsole.csproj

--- src/TestConsole/Program.cs
+++ src/TestConsole/Program.cs
@@ -6,13 +6,13 @@ builder.SetBasePath(Directory.GetCurrentDirectory())
        .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true);
 IConfigurationRoot configuration = builder.Build();
 
-await TestAPI.RunCallApiAsync(configuration);
+// await TestAPI.RunCallApiAsync(configuration);
 
 // await TestAgentClassic.RunAIFoundryTestVectorStoreAsync(configuration);
 // await TestAgentClassic.RunAIFoundryTestCodeInterpreterAsync(configuration);
 // await TestAgentClassic.CleanUpAIFoundryProjectAsync(configuration);
 
 // await TestAgentNew.RunFileSearchToolTestAsync(configuration);
-// await TestAgentNew.RunImageAnalysisToolTestAsync(configuration);
+await TestAgentNew.RunImageAnalysisToolTestAsync(configuration);
 // await TestAgentNew.RunFileAnalysisToolTestAsync(configuration);
 // await TestAgentNew.RunBasicAgentInfoTestAsync(configuration);
